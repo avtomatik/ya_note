95840a2c35113c17f0976910ee7bf93fdd896041
Alexander <kkruscherr@gmail.com>
2024-06-15 21:24:09 +0300

    Best Attempt of Tests So Far

---
+++ notes/tests/test_routes.py
@@ -0,0 +1,45 @@
+from http import HTTPStatus
+
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.urls import reverse
+
+from notes.models import Note
+
+User = get_user_model()
+
+
+class TestRoutes(TestCase):
+
+    @classmethod
+    def setUpTestData(cls) -> None:
+        cls.author = User.objects.create(username='Ludwig Wittgenstein')
+        cls.note = Note.objects.create(
+            title='Заголовок',
+            text='Текст',
+            slug='Slug',
+            author=cls.author
+        )
+
+    def test_pages_availability(self):
+        urls = (
+            ('notes:home', None),
+            ('notes:detail', (self.note.slug,)),
+            ('users:login', None),
+            ('users:logout', None),
+            ('users:signup', None),
+        )
+        for name, args in urls:
+            with self.subTest(name=name):
+                url = reverse(name, args=args)
+                response = self.client.get(url)
+                self.assertEqual(response.status_code, HTTPStatus.OK)
+
+    def test_redirect_for_anonymous_client(self):
+        login_url = reverse('users:login')
+        for name in ('notes:edit', 'notes:delete'):
+            with self.subTest(name=name):
+                url = reverse(name, args=(self.note.slug,))
+                redirect_url = f'{login_url}?next={url}'
+                response = self.client.get(url)
+                self.assertRedirects(response, redirect_url)
