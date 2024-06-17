03e6ebb0f403d8978b50e04a09ea503e8370f7db
Alexander <kkruscherr@gmail.com>
2024-06-17 21:47:14 +0300

    Added Routes' Tests

---
+++ notes/pytest_tests/test_routes.py
@@ -0,0 +1,105 @@
+from http import HTTPStatus
+
+import pytest
+from django.urls import reverse
+from pytest_django.asserts import assertRedirects
+
+
+def test_home_availability_for_anonymous_user(client):
+    """Главная страница доступна анонимному пользователю."""
+    url = reverse('notes:home')
+    response = client.get(url)
+    assert response.status_code == HTTPStatus.OK
+
+
+@pytest.mark.parametrize(
+    'name',
+    ('notes:list', 'notes:add', 'notes:success')
+)
+def test_pages_availability_for_auth_user(not_author_client, name):
+    """
+    Аутентифицированному пользователю доступна страница со списком заметок
+    notes/, страница успешного добавления заметки done/, страница добавления
+    новой заметки add/.
+
+    """
+    url = reverse(name)
+    response = not_author_client.get(url)
+    assert response.status_code == HTTPStatus.OK
+
+
+# =============================================================================
+# Страницы отдельной заметки, удаления и редактирования заметки доступны
+# только автору заметки. Если на эти страницы попытается зайти другой
+# пользователь — вернётся ошибка 404.
+# =============================================================================
+
+
+@pytest.mark.parametrize(
+    'name',
+    ('notes:home', 'users:login', 'users:logout', 'users:signup')
+)
+def test_pages_availability_for_anonymous_user(client, name):
+    """
+    Страницы регистрации пользователей, входа в учётную запись и выхода из неё
+    доступны всем пользователям.
+
+    """
+    url = reverse(name)
+    response = client.get(url)
+    assert response.status_code == HTTPStatus.OK
+
+
+@pytest.mark.parametrize(
+    'name',
+    ('notes:detail', 'notes:edit', 'notes:delete')
+)
+def test_pages_availability_for_author(author_client, name, note):
+    url = reverse(name, args=(note.slug,))
+    response = author_client.get(url)
+    assert response.status_code == HTTPStatus.OK
+
+
+@pytest.mark.parametrize(
+    'parametrized_client, expected_status',
+    (
+        (pytest.lazy_fixture('not_author_client'), HTTPStatus.NOT_FOUND),
+        (pytest.lazy_fixture('author_client'), HTTPStatus.OK)
+    ),
+)
+@pytest.mark.parametrize(
+    'name',
+    ('notes:detail', 'notes:edit', 'notes:delete'),
+)
+def test_pages_availability_for_different_users(
+    parametrized_client, name, note, expected_status
+):
+    url = reverse(name, args=(note.slug,))
+    response = parametrized_client.get(url)
+    assert response.status_code == expected_status
+
+
+@pytest.mark.parametrize(
+    'name, note_object',
+    (
+        ('notes:detail', pytest.lazy_fixture('slug_for_args')),
+        ('notes:edit', pytest.lazy_fixture('slug_for_args')),
+        ('notes:delete', pytest.lazy_fixture('slug_for_args')),
+        ('notes:add', None),
+        ('notes:success', None),
+        ('notes:list', None),
+    ),
+)
+def test_redirects(client, name, args):
+    """
+    При попытке перейти на страницу списка заметок, страницу успешного
+    добавления записи, страницу добавления заметки, отдельной заметки,
+    редактирования или удаления заметки анонимный пользователь
+    перенаправляется на страницу логина.
+
+    """
+    login_url = reverse('users:login')
+    url = reverse(name, args=args)
+    expected_url = f'{login_url}?next={url}'
+    response = client.get(url)
+    assertRedirects(response, expected_url)
